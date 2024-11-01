ba3ed420963b236d18548d68151a990a7f11b508
Rhythm Bansal <rskbansal@outlook.com>
2024-11-01 14:33:56 +0530

    made errors comprehensive

--- ai4bharat/errors.py
+++ ai4bharat/errors.py
@@ -1,5 +1,15 @@
+from constants import *
+
 class LanguageError(Exception):
-    pass
+    def __init__(self, lang_origin, lang):
+        # message = f'Invalid {lang_origin} language - "{lang}", please chose from the list below:\n\n{", ".join(LANGUAGES.keys())}'
+        message = f'Invalid {lang_origin} language - "{lang}"'
+        super().__init__(message)
 
 class DomainError(Exception):
     pass
+
+class VoiceError(Exception):
+    def __init__(self, voice):
+        message = f'Invalid voice - "{voice}", please chose between "male" or "female"'
+        super().__init__(message)
