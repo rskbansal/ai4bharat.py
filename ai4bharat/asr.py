8d9a02696920e9fe1ec9381ca94e0a35b9d05784
Rhythm Bansal <rskbansal@outlook.com>
2024-11-01 14:42:48 +0530

    added code for ASR

--- ai4bharat/asr.py
+++ ai4bharat/asr.py
@@ -1,2 +1,37 @@
 from constants import *
+from errors import *
 from utils import *
+import requests
+import base64
+
+def asr(audio_file, source_lang, domain = 'general'):
+    source_lang = source_lang.strip().lower()
+    domain = domain.strip().lower()
+
+    with open(audio_file, 'rb') as f:
+        data = f.read()
+
+    if LANGUAGES.get(source_lang) == None or source_lang == 'rajasthani':
+        raise LanguageError('source', source_lang)
+    if domain not in ASR_DOMAINS:
+        raise DomainError(domain)
+    
+    sampling_rate = get_sampling_rate(audio_file)
+    
+    payload = {
+        'sourceLanguage': LANGUAGES[source_lang]['code'],
+        'audioContent': base64.b64encode(data).decode(),
+        'task': 'asr',
+        'domain': domain,
+        'serviceId': 'ai4bharat/conformer-multilingual-all--gpu-t4' if source_lang != 'english' else 'ai4bharat/whisper--gpu-t4',
+        'samplingRate': sampling_rate,
+        'preProcessors': [],
+        'postProcessors': [],
+        'track': False
+    }
+
+    resp = requests.post(ASR_ENDPOINT, json=payload)
+    resp = resp.json()
+    print(resp)
+
+    return None
