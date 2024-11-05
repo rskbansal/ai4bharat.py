f0e14672390eef94568f7b7a8b7d9c601b4fa288
Rhythm Bansal <rskbansal@outlook.com>
2024-11-05 20:21:30 +0530

    added code for generating filename

--- ai4bharat/utils.py
+++ ai4bharat/utils.py
@@ -1,5 +1,10 @@
-import soundfile 
+from datetime import datetime
+import soundfile
 
 def get_sampling_rate(file_path):
     info = soundfile.info(file_path)
     return info.samplerate
+
+def gen_tts_filename(language):
+    curr_date_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')   
+    return f'TTS_{language}_{curr_date_time}.wav'
