f1c151e30a163c7ce09cb6f5111b0dc3c2c33090
Rhythm Bansal <rskbansal@outlook.com>
2024-11-05 20:23:38 +0530

    added code for TTS

--- ai4bharat/tts.py
+++ ai4bharat/tts.py
@@ -1,2 +1,44 @@
 from constants import *
+from errors import *
 from utils import *
+import requests
+import base64
+
+def tts(text, source_lang, out_file = None, voice = 'male', sampling_rate = 16000):
+    source_lang = source_lang.strip().lower()
+    voice = voice.strip().lower()
+    sampling_rate = int(sampling_rate)
+
+    # Handle minimum value for sampling rate
+
+    if LANGUAGES.get(source_lang) == None or LANGUAGES[source_lang]['tts_service'] == None:
+        raise LanguageError('source', source_lang)
+    if voice != 'male' and voice != 'female':
+        raise VoiceError(voice)
+    
+    payload = {
+        'sourceLanguage': LANGUAGES[source_lang]['code'],
+        'input': text,
+        'task': 'tts',
+        'serviceId': LANGUAGES[source_lang]['tts_service'],
+        'samplingRate': sampling_rate,
+        'gender': voice,
+        'track': False
+    }
+
+    resp = requests.post(TTS_ENDPOINT, json = payload)
+    status_code = resp.status_code
+
+    if status_code != 200:
+        raise APIError(status_code)
+    
+    resp = resp.json()
+    audio_data = resp['audio'][0]['audioContent']
+
+    if out_file == None:
+        out_file = gen_tts_filename(source_lang)
+
+    with open(out_file, 'wb') as f:
+        f.write(base64.b64decode(audio_data))
+
+    return None
