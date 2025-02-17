4ab7f5a66a07db83626a9d13ddf700758b2735de
Rhythm Bansal <rskbansal@outlook.com>
2025-02-18 01:28:45 +0530

    added docstrings for utility functions

--- ai4bharat/utils.py
+++ ai4bharat/utils.py
@@ -2,9 +2,25 @@ from datetime import datetime
 import soundfile
 
 def get_sampling_rate(file_path):
+    """Returns the sampling rate of an audio file.
+
+    Arguments:
+        file_path (str): Path to the audio file.
+
+    Returns:
+        int: The parsed sampling rate.
+    """
     info = soundfile.info(file_path)
     return info.samplerate
 
 def gen_tts_filename(language):
+    """Generates a filename for the TTS output audio file, incase not provided by the user.
+
+    Arguments:
+        language (str): The language of the speech/text.
+
+    Returns:
+        str: The generated filename in the format 'TTS_{language}_{current_date_time}.wav'.
+    """
     curr_date_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')   
     return f'TTS_{language}_{curr_date_time}.wav'
